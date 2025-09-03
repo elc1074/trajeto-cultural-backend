3bc19160b9783e02186552813a0b4ea7f74024a0
Renan Bordignon Poy <renanbp2@gmail.com>
2025-09-03 18:16:24 -0300

    update cors

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask
-from flask_cors import CORS
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.usuario import router as usuario_router
 from routers.acervo import router as acervo_router
 
-app = Flask(__name__)
-CORS(app)
 app = FastAPI(title="Trajeto cultural", version="0.1.0")
 
+origins = [
+    "http://localhost:5173",
+    "https://trajeto-cultural-frontend.vercel.app",
+    "https://trajeto-cultural-backend.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Rotas
 app.include_router(usuario_router)
 app.include_router(acervo_router)
 
