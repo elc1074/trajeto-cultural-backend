f9e67233ecc4c0a3a06c2ad9e97e825f6f35a9c8
Renan Bordignon Poy <renanbp2@gmail.com>
2025-10-29 21:15:07 -0300

    fix gemini router

--- routers/gemini.py
+++ routers/gemini.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 import google.generativeai as genai
 import os
 import requests
@@ -7,11 +8,14 @@ router = APIRouter(prefix="/analise", tags=["analise"])
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
+class ImageInput(BaseModel):
+    image_url: str
+
 @router.post("/gemini")
-def comentar_obra(image_url: str):
+def comentar_obra(input_data: ImageInput):
     try:
         model = genai.GenerativeModel("gemini-2.5-flash")
-        image_bytes = requests.get(image_url).content
+        image_bytes = requests.get(input_data.image_url).content
         response = model.generate_content([
             {"mime_type": "image/jpeg", "data": image_bytes},
             "Faça uma breve análise artística e histórica da imagem acima, em português, com no máximo 3 frases."
